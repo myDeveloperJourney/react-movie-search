c74d549052c58be107e76a263d3c816e9b79647a
Daniel JS <daniel@mydeveloperjourney.com>
2022-09-16 12:34:00 -0500

    wip: adds refinements

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 
 function Form({ getMovieData }) {
-    const [formData, setFormData] = useState({ searchTerm: "" });
+    const initialState = { searchTerm: "" };
+
+    const [formData, setFormData] = useState(initialState);
     
     const handleChange = (event) => {
         setFormData({ searchTerm: event.target.value });
@@ -9,17 +11,24 @@ function Form({ getMovieData }) {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        if(formData.searchTerm === "") return; 
+        // this prevents prevent the below code from running
         getMovieData(formData.searchTerm);
+        setFormData(initialState);
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input 
-                    type="text" 
-                    value={formData.searchTerm} 
-                    onChange={handleChange} 
-                />
+                <label>Movie Title
+                    <input
+                        required 
+                        type="text" 
+                        value={formData.searchTerm} 
+                        onChange={handleChange}
+                        placeholder="Blade Runner 2049 ... etc" 
+                    />
+                </label>
                 <input type="submit" value="Submit" />
             </form>
         </div>
--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -1,14 +1,21 @@
 function MovieDisplay(props) {
-    if(props.movie !== null) {
+    
+    const loaded = () => {
         return (
             <>
                 <h2>{props.movie.Title}</h2>
+                <h2>{props.movie.Genre}</h2>
                 <img src={props.movie.Poster} alt={props.movie.Title} />
+                <h2>{props.movie.Year}</h2>
             </>
         );
-    } else {
+    }
+
+    const notLoaded = () => {
         return <h2>Please Search For A Movie</h2>;
     }
+
+    return props.movie ? loaded() : notLoaded();
 }
 
 export default MovieDisplay;
