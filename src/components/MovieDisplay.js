6d194f1d445a205fb3074cf52d8692f1ed92c4f9
Daniel JS <daniel@mydeveloperjourney.com>
2022-09-16 12:42:33 -0500

    wip: adds notfound feature

--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -9,13 +9,22 @@ function MovieDisplay(props) {
                 <h2>{props.movie.Year}</h2>
             </>
         );
-    }
+    };
 
     const notLoaded = () => {
         return <h2>Please Search For A Movie</h2>;
-    }
+    };
 
-    return props.movie ? loaded() : notLoaded();
+    const notFound = () => {
+        return (
+            <>
+                <h2>Sorry, We Couldn't Find That</h2>
+                <p>Please check your spelling and try again</p>
+            </>
+        );
+    };
+
+    return props.movie ? props.movie.Error ? notFound() : loaded() : notLoaded();
 }
 
 export default MovieDisplay;
